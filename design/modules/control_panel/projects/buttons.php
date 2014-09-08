e069c8c0cd7b254135155347f81c95dec85ca1f5
Ilya Vorozhbit <ilya@mctop.im>
2014-09-08 13:06:11 +0400

    Кнопки для проектов
    :)

---
+++ design/modules/control_panel/projects/buttons.php
@@ -0,0 +1,9 @@
+
+    <h1>Кнопки для проекта</h1><hr>
+
+    <form role="form">
+        <?php
+            for($i=1; $i<=10; $i++)
+                require('buttons/small_banner.php');
+        ?>
+    </form>
--- design/modules/control_panel/projects/view.php
+++ design/modules/control_panel/projects/view.php
@@ -14,8 +14,10 @@
 
                 <a onclick="display_page_with_id('control_panel', 'projects/update', '<?php echo $project->id?>')" class="btn btn-primary">Обновить сведения</a>
                 <a onclick="display_page_with_id('control_panel', 'projects/access_settings', '<?php echo $project->id?>')" class="btn btn-primary">Настройки доступа</a>
+                <a onclick="display_page_with_id('control_panel', 'projects/buttons', '<?php echo $project->id?>')" class="btn btn-primary">Кнопки для проекта</a>
                 <a onclick="display_page_with_id('control_panel', 'servers/create', '<?php echo $project->id?>')" class="btn btn-success">Добавить сервер</a>
                 <a onclick="display_page_with_id('control_panel', 'projects/remove', '<?php echo $project->id?>')" class="btn btn-primary">Удаление проекта</a>
+
 
                 <div class="servers">
                     <?php
