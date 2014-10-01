d20a5c277bf7d46a1dc0c8004d953ac5cd88a7a3
Ilya Vorozhbit <ilya@mctop.im>
2014-10-02 03:36:19 +0400

    Some search

---
+++ api/Search.php
@@ -0,0 +1,64 @@
+<?php
+
+function array_php2pg($array, $data_type = 'character varying')
+{
+    $array = (array) $array; // Type cast to array.
+    $result = [];
+
+    foreach ($array as $entry)
+    { // Iterate through array.
+        if (is_array($entry)) // Supports nested arrays.
+        {
+            $result[] = array_php2pg($entry, $data_type);
+            continue;
+        }
+
+        $entry = str_replace('"', '\\"', $entry); // Escape double-quotes.
+        $entry = pg_escape_string($entry); // Escape everything else.
+
+        if (is_numeric($entry))
+            $result[] = $entry;
+        else
+            $result[] = '"' . $entry . '"';
+    }
+
+    $ret = '{' . implode(',', $result) . '}';
+
+    if ($data_type !== null)
+        $ret .= '::' . $data_type . '[]'; // format
+    return $ret;
+}
+
+class Search extends api
+{
+
+    protected function reserve()
+    {
+        return [
+            "design" => "search/index",
+            "script" => ["libs/chosen.jquery", "libs/jquery.tagsinput"],
+        ];
+    }
+
+    protected function get_results($tags_string, $version = 0)
+    {
+        phoxy_protected_assert(!$version, []);
+        $tags = explode(',', $tags_string);
+        foreach ($tags as &$tag)
+            if (strlen($tag))
+                $tag = "%$tag%";
+
+        $query = 'select * from main.servers where tags like all ($1::varchar[]) order by votes desc';
+
+        $pg = array_php2pg($tags, null);
+
+        $servers = Core::get_db()->Query($query,[$pg]);
+
+        return
+        [
+            "data" => ["search" => $servers],
+            "result" => "search_out",
+            "design" => "search/server_result",
+        ];
+    }
+}
