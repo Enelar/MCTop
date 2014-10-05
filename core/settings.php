4dcd52d2d8ce4cacb67df22c0a984a493e25c904
Kirill Berezin <enelar@develop-project.ru>
2014-10-05 19:16:29 +0400

    Clearify settings

--- core/settings.php
+++ core/settings.php
@@ -9,134 +9,126 @@ class Settings
 
     public function __construct()
     {
-        $settings = array(
-            'databases' => array(
-                'db' => array(
-                    'postgres' => array(
+        $settings =
+        [
+            'databases' =>
+            [
+                'db' =>
+                [
+                    'postgres' =>
+                    [
                         'server_address'    => '127.0.0.1',
                         'server_port'       => '5432',
                         'user_name'         => 'postgres',
                         'password'          => '',
                         'working_base_name' => 'mctop',
-                    ),
-                    'redis' => array(
+                    ],
+                    'redis' =>
+                    [
                         'server_address' => '127.0.0.1',
                         'using_password' => false,
                         'user_name' => 'root',
                         'redis_server_port' => 6379,
                         'db_number' => 2,
-                    )
-                ),
-            ),
+                    ],
+                ],
+            ],
 
-            'modules' => array(
-                'main' => new Object(
-                        array(
-                            'show_in_navbar' => false
-                        )
-                    ),
-                'news' => new Object(
-                        array(
-                            'show_in_navbar' => true,
-                            'show_to_authorized' => true,
-                            'show_to_guest' => false,
-                            'name' => 'Новости',
-                            'icon' => 'glyphicon glyphicon-volume-up',
-                            'link' => '/News'
-                        )
-                    ),
-                'projects_rating' => new Object(
-                        array(
-                            'show_in_navbar' => true,
-                            'show_to_authorized' => true,
-                            'show_to_guest' => true,
-                            'name' => 'Проекты',
-                            'icon' => 'glyphicon glyphicon-star',
-                            'link' => '/'
-                        )
-                    ),
-                'rating' => new Object(
-                        array(
-                            'show_in_navbar' => true,
-                            'show_to_authorized' => true,
-                            'show_to_guest' => false,
-                            'name' => 'Серверы',
-                            'icon' => 'glyphicon glyphicon-home',
-                            'link' => '/Servers/list'
-                        )
-                    ),
-                'search' => new Object(
-                        array(
-                            'show_in_navbar' => true,
-                            'show_to_authorized' => true,
-                            'show_to_guest' => true,
-                            'name' => 'Поиск',
-                            'icon' => 'glyphicon glyphicon-search',
-                            'link' => '/Search'
-                        )
-                    ),
-                'outdoor' => new Object(
-                        array(
-                            'show_in_navbar' => true,
-                            'show_to_authorized' => false,
-                            'show_to_guest' => true,
-                            'name' => 'Авторизация',
-                            'icon' => 'glyphicon glyphicon-lock',
-                            'link' => '/Users/login_page'
-                        )
-                    ),
-                'registration' => new Object(
-                        array(
-                            'show_in_navbar' => true,
-                            'show_to_authorized' => false,
-                            'show_to_guest' => true,
-                            'name' => 'Регистрация',
-                            'icon' => 'glyphicon glyphicon-plus',
-                            'link' => '/Users/register_page'
-                        )
-                    ),
-                'social' => new Object(
-                        array(
-                            'show_in_navbar' => false
-                        )
-                    ),
-                'control_panel' => new Object(
-                        array(
-                            'show_in_navbar' => true,
-                            'show_to_authorized' => true,
-                            'show_to_guest' => false,
-                            'name' => 'Панель управления',
-                            'icon' => 'glyphicon glyphicon-plus',
-                            'link' => '/ControlPanel'
-                        )
-                    ),
-                'why' => new Object(
-                        array(
-                            'show_in_navbar' => true,
-                            'show_to_authorized' => false,
-                            'show_to_guest' => true,
-                            'icon' => 'glyphicon glyphicon-repeat',
-                            'name' => 'Почему?',
-                            'link' => '/Main/why'
-                        )
-                    ),
-                'api' => new Object(
-                        array(
-                            'show_in_navbar' => false,
-                            'icon' => 'glyphicon glyphicon-picture',
-                        )
-                ),
-            ),
+            'modules' =>
+            [
+                'main' =>
+                [
+                    'show_in_navbar' => false,
+                ],
+                'news' =>
+                [
+                    'show_in_navbar' => true,
+                    'show_to_authorized' => true,
+                    'show_to_guest' => false,
+                    'name' => 'Новости',
+                    'icon' => 'glyphicon glyphicon-volume-up',
+                    'link' => '/News',
+                ],
+                'projects_rating' =>
+                [
+                    'show_in_navbar' => true,
+                    'show_to_authorized' => true,
+                    'show_to_guest' => true,
+                    'name' => 'Проекты',
+                    'icon' => 'glyphicon glyphicon-star',
+                    'link' => '/',
+                ],
+                'rating' =>
+                [
+                    'show_in_navbar' => true,
+                    'show_to_authorized' => true,
+                    'show_to_guest' => false,
+                    'name' => 'Серверы',
+                    'icon' => 'glyphicon glyphicon-home',
+                    'link' => '/Servers/list',
+                    ],
+                'search' =>
+                [
+                    'show_in_navbar' => true,
+                    'show_to_authorized' => true,
+                    'show_to_guest' => true,
+                    'name' => 'Поиск',
+                    'icon' => 'glyphicon glyphicon-search',
+                    'link' => '/Search',
+                ],
+                'outdoor' =>
+                [
+                    'show_in_navbar' => true,
+                    'show_to_authorized' => false,
+                    'show_to_guest' => true,
+                    'name' => 'Авторизация',
+                    'icon' => 'glyphicon glyphicon-lock',
+                    'link' => '/Users/login_page',
+                ],
+                'registration' =>
+                [
+                    'show_in_navbar' => true,
+                    'show_to_authorized' => false,
+                    'show_to_guest' => true,
+                    'name' => 'Регистрация',
+                    'icon' => 'glyphicon glyphicon-plus',
+                    'link' => '/Users/register_page',
+                ],
+                'social' =>
+                [
+                    'show_in_navbar' => false,
+                ],
+                'control_panel' =>
+                [
+                    'show_in_navbar' => true,
+                    'show_to_authorized' => true,
+                    'show_to_guest' => false,
+                    'name' => 'Панель управления',
+                    'icon' => 'glyphicon glyphicon-plus',
+                    'link' => '/ControlPanel',
+                ],
+                'why' =>
+                [
+                    'show_in_navbar' => true,
+                    'show_to_authorized' => false,
+                    'show_to_guest' => true,
+                    'icon' => 'glyphicon glyphicon-repeat',
+                    'name' => 'Почему?',
+                    'link' => '/Main/why',
+                ],
+                'api' =>
+                [
+                    'show_in_navbar' => false,
+                    'icon' => 'glyphicon glyphicon-picture',
+                ],
+            ],
 
-            'application' => [
-
+            'application' =>
+            [
                 'site_domain' => 'mctop.im',
-
                 'site_name' => 'MCTop',
                 'title_content' => 'MCTop - Minecraft сервера',
-
                 'game' => 'Minecraft',
-
                 'official_game_site' => 'http://minecraft.net',
 
                 'official_site_vay_club' => '',
@@ -147,11 +139,12 @@ class Settings
                 'official_game_facebook_public' => '',
                 'official_game_vk_public' => '',
 
-                'friends' => [
+                'friends' =>
+                [
                     'http://topcraft.ru' => '...',
                     'http://minecraft-servera.ru' => '...',
                     'http://want2vote.com' => '...',
-                    'http://mctop.ru/friends' => 'Стать другом'
+                    'http://mctop.ru/friends' => 'Стать другом',
                 ],
 
                 'official_wiki' => '',
@@ -160,12 +153,13 @@ class Settings
                 'image_prefix' => "./../img/",
             ],
 
-            'engine' => [
+            'engine' =>
+            [
                 'name' => 'VoP', // (Vay on Phoxy)
                 'version' => '0.2',
             ],
 
-        );
+        ];
 
         $this->db = $settings['databases']['db'];
         $this->application = $settings['application'];
