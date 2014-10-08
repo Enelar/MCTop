761053617530516435ccd134c58465d0415c3356
IV <ilya@mctop.im>
2014-10-08 21:33:54 +0400

    Faq is completed

--- api/Faq.php
+++ api/Faq.php
@@ -28,11 +28,26 @@ class Faq extends api {
 		$pages = Core::get_db()->Query('select * from main.faq_pages where category = $1', [$id]);
 		return
 		[
-			'design' => 'main/pages/faq/category',
+			'design' => 'main/pages/faq/category_pages',
 			'data' => [
 				'pages' => $pages,
 			],
 		];		
+	}
+
+	protected function page($id)
+	{
+		$page = Core::get_db()->Query('select * from main.faq_pages where id = $1', [$id], true);
+		$category = Core::get_db()->Query('select * from main.faq_categories where id = $1', [$id], true);
+		return
+		[
+			'design' => 'main/pages/faq/page',
+			'data' => 
+			[
+				'page' => $page,
+				'category_info' => $category
+			],
+		];
 	}
 
 }
